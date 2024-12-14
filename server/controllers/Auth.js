36081f31e4371e369555f2da9dbe6d0d0e8945e3
Dasvir <dasvirsingh07@gmail.com>
2024-12-14 22:30:05 +0530

    changed auth.js

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -4,7 +4,7 @@ const User=require("../models/User");
 const OTP=require("../models/OTP");
 const Profile=require("../models/Profile");
 const otpGenerator=require("otp-generator");
-const bcrypt=require('bcrypt');
+const bcrypt=require('bcryptjs');
 const mailSender = require("../utils/mailSender");
 const jwt=require("jsonwebtoken");
 require("dotenv").config();
