6787cb0154dada102e49944b895839d295d96cd4
Dasvir275 <115365920+Dasvir275@users.noreply.github.com>
2024-12-18 14:57:58 +0530

    Update Footer.jsx

--- Frontend/src/components/Common/Footer.jsx
+++ Frontend/src/components/Common/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FooterLink2 } from "../../data/footer-links";
 import { Link } from "react-router-dom";
 // Images
-import Logo from "../images/cwc.gif";
+import Logo from "../../images/cwclogo.gif";
 // Icons
 import { FaFacebook, FaGoogle, FaTwitter, FaYoutube } from "react-icons/fa";
 const BottomFooter = ["Privacy Policy", "Cookie Policy", "Terms"];
