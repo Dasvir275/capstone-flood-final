612f20fdfac8cc6becdde46fedafffcef8972e46
Dasvir275 <115365920+Dasvir275@users.noreply.github.com>
2024-12-18 13:30:03 +0530

    Update authAPI.js

--- Frontend/src/services/operations/authAPI.js
+++ Frontend/src/services/operations/authAPI.js
@@ -105,11 +105,11 @@ export function login(email, password, navigate) {
         : `https://api.dicebear.com/5.x/initials/svg?seed=${response.data.user.firstName} ${response.data.user.lastName}`
       dispatch(setUser({ ...response.data.user, image: userImage }))
       localStorage.setItem("token", JSON.stringify(response.data.token))
-      if(email==="dasvirsingh07@gmail.com"){
+      if(email==="dasvirsingh07@gmail.com" || email==="shivamaggarwal10b@gmail.com"){
         navigate("/main")
       }
       else{
-        navigate("/admindashboard")
+        navigate("/liveupdate")
       }
     } catch (error) {
       console.log("LOGIN API ERROR............", error)
